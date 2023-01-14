c0dc1ae53f8dc1658bc3c4e982a79248249fdfb6
John Ellis <532789+deckerego@users.noreply.github.com>
2023-01-14 18:28:15 -0500

    Automatically reconnect if we lose connectivity. Probably.

--- src/code.py
+++ src/code.py
@@ -4,6 +4,7 @@ import socketpool
 import ampule
 import light
 import led
+import time
 
 headers = {
     "Content-Type": "application/json; charset=UTF-8",
@@ -46,6 +47,37 @@ def _to_json(r, g, b, bright):
     hostname = secrets["hostname"] if "hostname" in secrets else wifi.radio.hostname
     return '{ "hostname": "%s", "red": %i, "green": %i, "blue": %i, "brightness": %f }' % (hostname, r, g, b, bright)
 
+def _wifi_connect(ssid, password):
+    max_retries = 3
+    sleep = 0.5
+    retries = 0
+    last_exception = BaseException("WiFi Connect Failure")
+
+    print("MAC: ", [hex(i) for i in wifi.radio.mac_address])
+    while retries <= max_retries:
+        try:
+            led.on()
+            print("Connecting to %s..." % ssid)
+            wifi.radio.connect(ssid, password)
+            return True
+        except Exception as e:
+            led.off()
+            print("Will retry connection to %s: %s" % (ssid, e))
+            last_exception = e
+            retries += 1
+            time.sleep(sleep * retries)
+    raise last_exception
+
+def _socket_build():
+    led.off()
+    pool = socketpool.SocketPool(wifi.radio)
+    socket = pool.socket()
+    socket.bind(['0.0.0.0', 7413])
+    socket.listen(1)
+    socket.setblocking(True)
+    print("IPv4 Addr:", wifi.radio.ipv4_address)
+    led.on()
+    return socket
 
 ### BEGIN Initialize Tally Light
 
@@ -58,25 +90,23 @@ except ImportError:
     led.blink(0.25)
     raise
 
-try:
-    print("Connecting to %s..." % secrets["ssid"])
-    print("MAC: ", [hex(i) for i in wifi.radio.mac_address])
-    wifi.radio.connect(secrets["ssid"], secrets["password"])
-except:
-    print("Error connecting to WiFi")
-    led.blink(0.1)
-    raise
-
-led.off()
-pool = socketpool.SocketPool(wifi.radio)
-socket = pool.socket()
-socket.bind(['0.0.0.0', 7413])
-socket.listen(1)
-socket.setblocking(True)
-print("Connected to %s, IPv4 Addr: " % secrets["ssid"], wifi.radio.ipv4_address)
-
-led.on()
-light.test()
-
+socket = None
 while True:
-    ampule.listen(socket)
+    if not wifi.radio.ap_info or not wifi.radio.ap_info.channel:
+        if socket: socket.close()
+
+        try:
+            _wifi_connect(secrets["ssid"], secrets["password"])
+        except Exception as e:
+            print("Error connecting to WiFi:", e)
+            led.blink(0.1)
+            raise
+
+        socket = _socket_build()
+        light.test()
+
+    if socket: 
+        ampule.listen(socket)
+    else:
+        print("No socket available!")
+        led.blink(0.5)
